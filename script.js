b6cb13d9e9602745fd9887fefd92fd4b70ded10f
Unesco <younes.tahiri6@gmail.com>
2022-06-04 23:25:00 +0100

    js and html files updated

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let div = `<div class="content">
+<input type="search" id="search" placeholder="Search...">
+<i class="fas fa-search"></i>
+</div>`;
+
+document.body.innerHTML += div;
+
 let icone = document.querySelector('i');
 let inp = document.querySelector('input');
 console.log(inp)
